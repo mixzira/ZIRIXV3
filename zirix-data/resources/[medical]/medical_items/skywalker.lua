f33fcd7aea5563c1914486ce4322d61d9c489310
acess02 <78604225+acess02@users.noreply.github.com>
2021-02-11 13:06:19 -0800

    Medical_items
    NUI para os Medicos pegar os itens

---
+++ zirix-data/resources/[medical]/medical_items/skywalker.lua
@@ -0,0 +1,31 @@
+local Tunnel = module("vrp","lib/Tunnel")
+local Proxy = module("vrp","lib/Proxy")
+vRP = Proxy.getInterface("vRP")
+
+local valores = {
+	{ item = "headblock", quantidade = 1, compra = 0 },
+	{ item = "gesso", quantidade = 1, compra = 0 },
+	{ item = "bandagem", quantidade = 1, compra = 0 },
+	{ item = "cinta", quantidade = 1, compra = 0 },
+}
+RegisterServerEvent("medical-pegar")
+AddEventHandler("medical-pegar",function(item)
+	local source = source
+	local user_id = vRP.getUserId(source)
+	if user_id then
+		for k,v in pairs(valores) do
+			if item == v.item then
+				if vRP.getInventoryWeight(user_id)+vRP.getItemWeight(v.item)*v.quantidade <= vRP.getInventoryMaxWeight(user_id) then
+					if vRP.tryPayment(user_id,parseInt(v.compra)) then
+						vRP.giveInventoryItem(user_id,v.item,parseInt(v.quantidade))
+						TriggerClientEvent("Notify",source,"sucesso","Pegou <b>"..parseInt(v.quantidade).."x "..vRP.itemNameList(v.item)..".")
+					else
+						TriggerClientEvent("Notify",source,"negado","Dinheiro insuficiente.")
+					end
+				else
+					TriggerClientEvent("Notify",source,"negado","Espaço insuficiente.")
+				end
+			end
+		end
+	end
+end)
