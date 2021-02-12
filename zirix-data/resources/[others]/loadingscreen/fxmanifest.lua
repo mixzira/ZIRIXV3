adfb14a4e39bc67d0e534cbc5f8c103fd868b01d
Mateus Magalhães <58696475+MixZira@users.noreply.github.com>
2021-02-12 15:17:32 -0300

    ZIRIX V3

---
+++ zirix-data/resources/[others]/loadingscreen/fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'MixZira'
+contact 'E-mail: mixzira@outlook.com.br - Discord: MixZira#0001'
+version '1.0.0'
+
+client_scripts {
+    '@vrp/lib/utils.lua',
+    'hansolo/*'
+}
+
+server_scripts {
+    '@vrp/lib/utils.lua',
+    'skywalker.lua'
+}
+
+files {
+    'nui/*',
+    'nui/*',
+    'nui/*',
+    'nui/**/*'
+}
+
+loadscreen 'nui/darkside.html'
+loadscreen_manual_shutdown "yes"
--- zirix-data/resources/[sistema]/[builders]/webpack/fxmanifest.lua
+++
@@ -1,5 +0,0 @@
-dependency 'yarn'
-server_script 'webpack_builder.js'
-
-fx_version 'adamant'
-game 'common'
