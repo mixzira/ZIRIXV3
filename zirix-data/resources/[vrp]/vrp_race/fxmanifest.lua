bfd0426e090cdff3de35373f20fa05e74e99f05c
Mateus Magalhães <58696475+MixZira@users.noreply.github.com>
2021-02-12 14:32:49 -0300

    RACE

---
+++ zirix-data/resources/[vrp]/vrp_race/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'adamant'
+game 'gta5'
+
+client_scripts {
+	'@vrp/lib/utils.lua',
+	'config/config.lua',
+	'hansolo/*.lua'
+}
+
+server_scripts {
+	'@vrp/lib/utils.lua',
+	'config/config.lua',
+	'skywalker.lua'
+}
+
+files {
+	'Azonix.woff'
+}
