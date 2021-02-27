1afbb9939e366da50d80c2866bcabcf27551a589
Mateus Magalhães <58696475+MixZira@users.noreply.github.com>
2021-02-27 15:46:49 -0300

    ZIRIXV3-Update de correção.

--- zirix-data/V3.sql
+++ zirix-data/V3.sql
@@ -1,197 +1,5 @@
-﻿/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
-CREATE DATABASE IF NOT EXISTS `zirix`;
+﻿CREATE DATABASE IF NOT EXISTS `zirix`;
 USE `zirix`;
-
-CREATE TABLE IF NOT EXISTS `phone_app_chat` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `channel` varchar(20) NOT NULL,
-  `message` varchar(255) NOT NULL,
-  `time` timestamp NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-CREATE TABLE IF NOT EXISTS `phone_calls` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `owner` varchar(10) NOT NULL COMMENT 'Meu número',
-  `num` varchar(10) NOT NULL COMMENT 'Número do contato',
-  `incoming` int(11) NOT NULL COMMENT 'Origem da chamada',
-  `time` timestamp NOT NULL DEFAULT current_timestamp(),
-  `accepts` int(11) NOT NULL COMMENT 'Chamada aceita ou não',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE IF NOT EXISTS `phone_messages` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `transmitter` varchar(10) NOT NULL,
-  `receiver` varchar(10) NOT NULL,
-  `message` varchar(255) NOT NULL DEFAULT '0',
-  `time` timestamp NOT NULL DEFAULT current_timestamp(),
-  `isRead` int(11) NOT NULL DEFAULT 0,
-  `owner` int(11) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-CREATE TABLE IF NOT EXISTS `phone_users_contacts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `identifier` varchar(60) CHARACTER SET utf8mb4 DEFAULT NULL,
-  `number` varchar(10) CHARACTER SET utf8mb4 DEFAULT NULL,
-  `display` varchar(64) CHARACTER SET utf8mb4 NOT NULL DEFAULT '-1',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-CREATE TABLE `bank_transfer` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`type` INT(11) NOT NULL,
-	`identifier` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`price` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`name` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`time` TIMESTAMP NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-	PRIMARY KEY (`id`) USING BTREE
-)
-COLLATE='utf8mb4_general_ci'
-ENGINE=InnoDB
-AUTO_INCREMENT=22
-;
-
-CREATE TABLE `yellow_tweets` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`phone_number` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_unicode_ci',
-	`firstname` VARCHAR(256) NULL DEFAULT NULL COLLATE 'utf8mb4_unicode_ci',
-	`lastname` VARCHAR(256) NULL DEFAULT NULL COLLATE 'utf8mb4_unicode_ci',
-	`message` VARCHAR(256) NOT NULL COLLATE 'utf8mb4_unicode_ci',
-	`image` VARCHAR(256) NULL DEFAULT NULL COLLATE 'utf8mb4_unicode_ci',
-	`time` TIMESTAMP NOT NULL DEFAULT current_timestamp(),
-	PRIMARY KEY (`id`) USING BTREE
-)
-COLLATE='utf8mb4_unicode_ci'
-ENGINE=InnoDB
-AUTO_INCREMENT=895
-;
-
-CREATE TABLE IF NOT EXISTS `twitter_accounts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) CHARACTER SET utf8 NOT NULL DEFAULT '0',
-  `password` varchar(64) COLLATE utf8mb4_bin NOT NULL DEFAULT '0',
-  `avatar_url` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
-CREATE TABLE IF NOT EXISTS `twitter_likes` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `authorId` int(11) DEFAULT NULL,
-  `tweetId` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_twitter_likes_twitter_accounts` (`authorId`),
-  KEY `FK_twitter_likes_twitter_tweets` (`tweetId`),
-  CONSTRAINT `FK_twitter_likes_twitter_accounts` FOREIGN KEY (`authorId`) REFERENCES `twitter_accounts` (`id`),
-  CONSTRAINT `FK_twitter_likes_twitter_tweets` FOREIGN KEY (`tweetId`) REFERENCES `twitter_tweets` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=137 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
-CREATE TABLE IF NOT EXISTS `twitter_tweets` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `authorId` int(11) NOT NULL,
-  `realUser` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `message` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `image` VARCHAR(256) NULL DEFAULT NULL COLLATE 'utf8mb4_unicode_ci',
-  `time` timestamp NOT NULL DEFAULT current_timestamp(),
-  `likes` int(11) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`),
-  KEY `FK_twitter_tweets_twitter_accounts` (`authorId`),
-  CONSTRAINT `FK_twitter_tweets_twitter_accounts` FOREIGN KEY (`authorId`) REFERENCES `twitter_accounts` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10702 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-CREATE TABLE `phone_ch_reddit` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`redgkit` VARCHAR(20) NOT NULL,
-	`time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY (`id`)
-)
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB
-;
-
-CREATE TABLE `phone_reddit` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`redgkit` VARCHAR(20) NULL DEFAULT NULL,
-	`reditsage` VARCHAR(255) NULL DEFAULT NULL,
-	`time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY (`id`)
-)
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB
-;
-
-CREATE TABLE IF NOT EXISTS `phone_shops` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `store` varchar(255) NOT NULL,
-  `item` varchar(100) NOT NULL,
-  `price` int(11) NOT NULL,
-  `label` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
-
-CREATE TABLE `insto_accounts` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`forename` VARCHAR(50) NOT NULL DEFAULT '0' COLLATE 'utf8mb4_bin',
-	`surname` VARCHAR(50) NOT NULL DEFAULT '0' COLLATE 'utf8mb4_bin',
-	`username` VARCHAR(50) NOT NULL DEFAULT '0' COLLATE 'utf8_general_ci',
-	`password` VARCHAR(50) NOT NULL DEFAULT '0' COLLATE 'utf8mb4_bin',
-	`avatar_url` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8mb4_bin',
-	PRIMARY KEY (`id`),
-	UNIQUE INDEX `username` (`username`)
-)
-COLLATE='utf8mb4_bin'
-ENGINE=InnoDB
-AUTO_INCREMENT=57
-;
-
-
-
-CREATE TABLE `insto_instas` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`authorId` INT(11) NOT NULL,
-	`realUser` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_unicode_ci',
-	`message` VARCHAR(256) NOT NULL COLLATE 'utf8mb4_unicode_ci',
-	`image` VARCHAR(256) NOT NULL COLLATE 'utf8mb4_unicode_ci',
-	`filters` VARCHAR(256) NOT NULL COLLATE 'utf8mb4_unicode_ci',
-	`time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`likes` INT(11) NOT NULL DEFAULT '0',
-	PRIMARY KEY (`id`),
-	INDEX `FK_insto_instas_insto_accounts` (`authorId`),
-	CONSTRAINT `FK_insto_instas_insto_accounts` FOREIGN KEY (`authorId`) REFERENCES `insto_accounts` (`id`)
-)
-COLLATE='utf8mb4_unicode_ci'
-ENGINE=InnoDB
-AUTO_INCREMENT=213
-;
-
-
-CREATE TABLE `insto_likes` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`authorId` INT(11) NULL DEFAULT NULL,
-	`inapId` INT(11) NULL DEFAULT NULL,
-	PRIMARY KEY (`id`),
-	INDEX `FK_insto_likes_insto_accounts` (`authorId`),
-	INDEX `FK_insto_likes_insto_instas` (`inapId`),
-	CONSTRAINT `FK_insto_likes_insto_accounts` FOREIGN KEY (`authorId`) REFERENCES `insto_accounts` (`id`),
-	CONSTRAINT `FK_insto_likes_insto_instas` FOREIGN KEY (`inapId`) REFERENCES `insto_instas` (`id`) ON DELETE CASCADE
-)
-COLLATE='utf8mb4_bin'
-ENGINE=InnoDB
-AUTO_INCREMENT=186
-;
-
-CREATE TABLE IF NOT EXISTS `vrp_estoque` (
-  `vehicle` varchar(100) NOT NULL,
-  `quantidade` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE IF NOT EXISTS `vrp_homes_permissions` (
   `owner` int(11) NOT NULL,
@@ -294,6 +102,11 @@ CREATE TABLE IF NOT EXISTS `vrp_stock_exchange` (
   `points` int(10) DEFAULT 100
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+CREATE TABLE IF NOT EXISTS `vrp_estoque` (
+  `vehicle` varchar(100) NOT NULL,
+  `quantidade` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 CREATE TABLE `web_login` (
   `id` int(11) NOT NULL,
   `login` varchar(30) NOT NULL,
@@ -303,7 +116,3 @@ CREATE TABLE `web_login` (
 
 INSERT INTO `web_login` (`id`, `login`, `senha`, `permissao`) VALUES
 (1, 'admin', 'admin', 99);
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
