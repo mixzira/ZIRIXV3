2639facb6f297f0d86a891c3adedcce14d9bfe0d
Mat0pi <61566701+Mat0pi@users.noreply.github.com>
2021-04-22 16:25:11 -0300

    vrp_itemdamage

--- zirix-data/resources/[vrp]/vrp_itemdamage/hansolo/hansolo.lua
+++ zirix-data/resources/[vrp]/vrp_itemdamage/hansolo/hansolo.lua
@@ -2,6 +2,8 @@ local Tunnel = module("vrp","lib/Tunnel")
 local Proxy = module("vrp","lib/Proxy")
 vRP = Proxy.getInterface("vRP")
 
+vSERVER = Tunnel.getInterface("vrp_itemdamage")
+
 Citizen.CreateThread(function()
 	while true do
 		local idle = 1000
@@ -10,10 +12,10 @@ Citizen.CreateThread(function()
 		if IsEntityInWater(ped) then
 			idle = 100
 			if IsPedSwimming(ped) and not IsPedSwimmingUnderWater(ped) then
-				misc.damageItem()
+				vSERVER.damageItem()
 			end
 			if IsPedSwimming(ped) and IsPedSwimmingUnderWater(ped) then
-				misc.damageItem()
+				vSERVER.damageItem()
 			end
 		end
 		Citizen.Wait(idle)
