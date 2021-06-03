ce8957454ea14025be862ee2a42a858a75258872
GermanBud <german@ziraflix.com>
2021-06-02 21:30:14 -0300

    still debugging

--- zirix-data/resources/[medical]/medical_system/hansolo/hansolo.lua
+++ zirix-data/resources/[medical]/medical_system/hansolo/hansolo.lua
@@ -37,9 +37,15 @@ function ToggleActionMenu()
 	end
 end
 
+local damaged = {}
+local bleeding = 0
+local bleedtype = "Superficial"
+
 RegisterNUICallback("ButtonClick",function(data,cb)
 	if data == "rem-kit" then
+		print('[DEBUG] diagnostic client: '.. json.encode(damaged, { indent = true }))
 		local head, legs, foot, breast = vSERVER.raiox()
+		print('[DEBUG] diagnostic server: '.. json.encode(vSERVER.raiox(), { indent = true }))
 		SendNUIMessage({
 			head = head,
 			legs = legs,
@@ -51,10 +57,6 @@ RegisterNUICallback("ButtonClick",function(data,cb)
 		ToggleActionMenu()
 	end
 end)
-
-local damaged = {}
-local bleeding = 0
-local bleedtype = "Superficial"
 
 local teste = false
 Citizen.CreateThread(function()
