5e5e89fd72555b9ee2baaf2a8f7229b7f77ee330
Eduardo <edu.ice7@gmail.com>
2022-02-16 21:18:46 +0000

    Update start.bat

--- zirix-data/start.bat
+++ zirix-data/start.bat
@@ -8,5 +8,5 @@ echo     Contact: contato@ziraflix.com
 echo ===-------------------------------===
 
 pause
-start ..\build\FXServer.exe +exec config\config.cfg +set onesync on +set onesync_population false
+start ..\build\FXServer.exe +exec config\config.cfg +set onesync_enableInfinity 1 onesync_enableBeyond 1 onesync_population 1 +set sv_enforceGameBuild tuner
 exit
