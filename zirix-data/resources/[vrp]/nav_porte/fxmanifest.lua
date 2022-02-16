18de71fd4a066562adc3012ecf2cfd5fd01431b7
Eduardo <edu.ice7@gmail.com>
2022-02-16 20:44:42 +0000

    fxmanifest.lua
    porte de arma

---
+++ zirix-data/resources/[vrp]/nav_porte/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'Ice41'
+contact 'E-mail: geral@ice41.pt'
+
+client_scripts {
+    '@vrp/lib/utils.lua',
+    'hansolo.lua'
+}
+
+server_scripts {
+    '@vrp/lib/utils.lua',
+    'skywalker.lua'
+}
